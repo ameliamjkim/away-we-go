26dddf2f03f7c8fa86e1c92cd4dea0c3ca1f67c9
Amelia Kim <ameliamjkim@gmail.com>
2018-11-05 10:55:30 -0500

    Add authorization

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,5 +1,14 @@
 class HomesController < ApplicationController
   before_action :authenticate_user!
+
   def index
+
+    if params[:id]
+      if Trip.exists?(params[:id]) && !Trip.find(params[:id]).users.include?(current_user)
+        redirect_to root_path
+      else
+        redirect_to root_path
+      end
+    end
   end
 end
