1c2166161addd0bb994a8afaa54eb6597ab60e26
Amelia Kim <ameliamjkim@gmail.com>
2018-11-18 18:28:30 -0500

    Chat testing

--- spec/models/chat_spec.rb
+++ spec/models/chat_spec.rb
@@ -1,13 +1,16 @@
 require 'rails_helper'
 
-describe Usertrip do
+describe Chat do
   let!(:user) { User.create(first_name: "Amelia", last_name: "Kim", email: "ameliamjkim@gmail.com", password: "helloeveryone") }
   let!(:trip) { Trip.create(name: "Greece", start_date: "11/01/2022", end_date: "11/12/2022", user: user ) }
 
-  it { should have_valid(:user).when(user) }
-  it { should_not have_valid(:user).when(nil) }
-
   it { should have_valid(:trip).when(trip) }
   it { should_not have_valid(:trip).when(nil) }
+
+  it { should have_valid(:title).when("Greece Chat") }
+  it { should have_valid(:title).when(nil, "") }
+
+  it { should have_valid(:description).when("Greece!") }
+  it { should have_valid(:description).when(nil, "") }
 
 end
